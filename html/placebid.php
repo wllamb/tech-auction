34a062dd3245decf2dbaed69f757f7168ad04cde
wllamb <wllamb@valdosta.edu>
2015-12-10 07:34:42 -0500

    User bidding
    User's can't bid on items they've listed now.

--- html/placebid.php
+++ html/placebid.php
@@ -1,25 +1,30 @@
 <?php
     require_once '../user_login/index.php';
     require_once 'dbconnect.php';
-
-    $auctionID = $_GET['id'];
-    $bidamt = trim($_POST['bid'], '$');
-    $bidderID = $_SESSION['user_id'];
-    $bidderName = rtrim($_SESSION['email'], '@gmail.com');
-    $result = $conn->query('SELECT * FROM itemlist WHERE id = '.$_GET['id']);
+    $canPlaceBid = true;
+    $auctionID = $_GET['id']; // get the auctions id value from database
+    $bidamt = trim($_POST['bid'], '$'); // bid amount placed by the user
+    $bidderID = $_SESSION['user_id']; //current bidders user id pulled from session data
+    $bidderName = rtrim($_SESSION['email'], '@gmail.com'); //bidder name taken from the email address with $email.com removed
+    $result = $conn->query('SELECT * FROM itemlist WHERE id = '.$_GET['id']); // find the item in the database
     $item = $result->fetch_assoc();
+    $ownerID = $_SESSION["user_id"];
+    //lets handle the case of user bidding on an item he/she listed
+    if($bidderID == $ownerID)
+    {
+      $canBid = false; //user cant bid
+    } else {
+      $canBid = true; // user can bid
+    }
     if ($item['bidnum'] == 0) {
         $bidIncrement = 1; //this is the first bid on the item...
     } else {
-        $bidIncrement = $item['bidnum'] + 1;
+        $bidIncrement = $item['bidnum'] + 1; //these are subsequent bids
     }
 
-    /******FUNCTION TO GET USERID THATS LOGGED IN******/
-    // 		Code...
-    //
-    /*************************************************/
 
-    if (($item['price'] < $bidamt) && ($item['hasended'] == 0) && ($_SESSION['logged_in'] == true)) {
+
+    if (($item['price'] < $bidamt) && ($item['hasended'] == 0) && ($_SESSION['logged_in'] == true) && ($canBid == true)) {
         $sql = "UPDATE itemlist SET price='".$bidamt."', bidderid='".$bidderID."', bidnum='".$bidIncrement."', biddername='".$bidderName."' WHERE id=".$auctionID;
         if ($conn->query($sql) === true) {
             echo 'Record updated successfully';
